74993d9c586c3c2f7bfb40c20689d1028500bb92
809949 <gunasrivinjam@gmail.com>
2023-11-09 15:04:47 +0530

    third commit

--- HelloWorldApp/views.py
+++ HelloWorldApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-
+def macha(request):
+    return HttpResponse('<marquee><h1>hi gopi k naidu</h1></marquee>')
 # Creating Custom Views
--- project1/urls.py
+++ project1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from HelloWorldApp.views import macha
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('macha',macha,name='macha')
 ]
